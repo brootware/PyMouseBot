dc40e0f21b0e45c38c946b598aa25c828a276d6d
rootware <ominbruce@outlook.com>
2022-03-02 10:30:24 +0800

    working setup

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ try:
 except ImportError:
     from distutils.core import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="pymousebot",
     version="1.0.0",
@@ -17,12 +20,14 @@ setup(
     author_email="ominbruce@outlook.com",
     maintainer="Oaker Min",
     maintainer_email="ominbruce@outlook.com",
+    long_description= long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/brootware/PyMouseBot.git",
     packages=['src'],
     platforms='any',
     py_modules=['mousebot'],
     entry_points='''
     [console_scripts]
-    mousebot=mousebot:main
+    pymousebot=mousebot:main
     '''
 )
