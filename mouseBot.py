1b20d173d442f6d7e47f49c4a127c6b504c10e3e
ominbruce <brucetheflame@outlook.com>
2022-03-02 16:04:12 +0800

    added ascii art

--- mouseBot.py
+++ mouseBot.py
@@ -12,6 +12,19 @@ import platform
 
 
 def runMouse(time_to_stop):
+    print("""\
+ ______          ______                            ______             
+(_____ \        |  ___ \                          (____  \       _    
+ _____) )   _   | | _ | | ___  _   _  ___  ____    ____)  ) ___ | |_  
+|  ____/ | | |  | || || |/ _ \| | | |/___)/ _  )  |  __  ( / _ \|  _) 
+| |    | |_| |  | || || | |_| | |_| |___ ( (/ /   | |__)  ) |_| | |__ 
+|_|     \__  |  |_||_||_|\___/ \____(___/ \____)  |______/ \___/ \___)
+       (____/                                                         
+                                          
+                +-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+-+-+
+                |p|o|w|e|r|e|d| |b|y| |r|o|o|t|w|a|r|e|
+                +-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+-+-+
+    """)
     condition_to_run = True
     while condition_to_run:
         # get current time
@@ -33,7 +46,8 @@ def runMouse(time_to_stop):
             from src.winMouse import winMoveClick
             winMoveClick(posx, posy)
         else:
-            print(f"[ - ] Seems like you are running on unsupported platform {platform.system()}. Currently the bot is only supported on Windows and Mac(Darwin).")
+            print(
+                f"[ - ] Seems like you are running on unsupported platform {platform.system()}. Currently the bot is only supported on Windows and Mac(Darwin).")
 
         if current_time > time_to_stop:
             iso_time = time.strftime("%Y-%m-%dT%H:%M:%SZ", time_to_stop)
@@ -46,12 +60,14 @@ def runMouse(time_to_stop):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("time", help="Please enter the time in HH:MM:SS format")
+    parser.add_argument(
+        "time", help="Please enter the time in HH:MM:SS format")
     args = parser.parse_args()
 
     try:
         current_date = time.strftime("%Y %m %d")
-        args.time = time.strptime(f"{current_date} {args.time}", "%Y %m %d  %H:%M:%S")
+        args.time = time.strptime(
+            f"{current_date} {args.time}", "%Y %m %d  %H:%M:%S")
     except ValueError:
         print(f"[ - ] The time you entered is incorrect. Try again in HH:MM:SS format")
 
