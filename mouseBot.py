977fea18b3eac5cec4fc9bc45ce6637f6e4801c6
rootware <ominbruce@outlook.com>
2022-03-04 08:50:51 +0800

    added instruction and handler on stopping the bot

--- mouseBot.py
+++ mouseBot.py
@@ -9,10 +9,12 @@ import time
 from random import randint
 import tkinter as tk
 import platform
+import sys
 
 
 def runMouse(time_to_stop):
-    print("""\
+    print(
+        """\
  ______          ______                            ______             
 (_____ \        |  ___ \                          (____  \       _    
  _____) )   _   | | _ | | ___  _   _  ___  ____    ____)  ) ___ | |_  
@@ -24,7 +26,9 @@ def runMouse(time_to_stop):
                 +-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+-+-+-+
                 |P|o|w|e|r|e|d| |b|y| |B|r|o|o|t|w|a|r|e|
                 +-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+-+-+-+
-    """)
+    """
+    print("If you want to stop the bot, press CTRL+C in the terminal")
+    )
     condition_to_run = True
     while condition_to_run:
         # get current time
@@ -41,16 +45,19 @@ def runMouse(time_to_stop):
 
         if platform.system() == "Darwin":
             from src.macMouse import macMoveClick
+
             macMoveClick(posx, posy)
         elif platform.system() == "Windows":
             from src.winMouse import winMoveClick
+
             winMoveClick(posx, posy)
         else:
             print(
-                f"[ - ] Seems like you are running on unsupported platform {platform.system()}. Currently the bot is only supported on Windows and Mac(Darwin).")
+                f"[ - ] Seems like you are running on unsupported platform {platform.system()}. Currently the bot is only supported on Windows and Mac(Darwin)."
+            )
 
         if current_time > time_to_stop:
-            iso_time = time.strftime("%Y-%m-%dT%H:%M:%SZ", time_to_stop)
+            iso_time = time.strftime("%H:%M:%S", time_to_stop)
             print(f"[ + ] It's after {iso_time} now, stopping this script.")
             condition_to_run = False
 
@@ -60,14 +67,12 @@ def runMouse(time_to_stop):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "time", help="Please enter the time in HH:MM:SS format")
+    parser.add_argument("time", help="Please enter the time in HH:MM:SS format")
     args = parser.parse_args()
 
     try:
         current_date = time.strftime("%Y %m %d")
-        args.time = time.strptime(
-            f"{current_date} {args.time}", "%Y %m %d  %H:%M:%S")
+        args.time = time.strptime(f"{current_date} {args.time}", "%Y %m %d  %H:%M:%S")
     except ValueError:
         print(f"[ - ] The time you entered is incorrect. Try again in HH:MM:SS format")
 
@@ -75,4 +80,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print(f"[ + ] The bot is stopped by the user. Good bye!")
+        sys.exit()
