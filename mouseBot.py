2d0399d587ae2e9fa590801b790007a33c6cee90
brootware <brootware@outlook.com>
2023-09-13 10:46:45 +0800

    using pyautogui for mouse movement now

--- mouseBot.py
+++ mouseBot.py
@@ -5,10 +5,9 @@ Utility to move Mouse
 
 import time
 import argparse
-from random import randint
-import tkinter as tk
-import platform
 import sys
+import pyautogui
+from datetime import datetime
 
 banner = """\
  ______          ______                            ______             
@@ -29,51 +28,39 @@ banner = """\
 
 def runMouse(time_to_stop, time_interval):
     print("If you want to stop the bot, press CTRL+C in the terminal")
+    pyautogui.FAILSAFE = False
+    
+    if time_interval is None:
+        time_interval=0.5
+    else:
+        time.sleep(float(time_interval))
 
     # Start of program
     condition_to_run = True
     while condition_to_run:
         # get current time
         current_time = time.localtime()
-        # get screen size
-        root = tk.Tk()
-        # TODO work on limiting the screen clicks to bottom right
-        screen_width = root.winfo_screenwidth() - 1000
-        screen_height = root.winfo_screenheight() - 500
-
-        # pass random value to axis
-        posx = randint(screen_height, screen_width)
-        posy = randint(screen_height, screen_width)
-
-        if platform.system() == "Darwin":
-            from src.macMouse import macMoveClick
-
-            macMoveClick(posx, posy)
-        elif platform.system() == "Windows":
-            from src.winMouse import winMoveClick
-
-            winMoveClick(posx, posy)
-        else:
-            print(
-                f"[ - ] Seems like you are running on unsupported platform {platform.system()}. Currently the bot is only supported on Windows and Mac(Darwin)."
-            )
+        x=0
+        while(x<time_interval):
+            time.sleep(60)
+            x+=1
+        for i in range(0,200):
+            pyautogui.moveTo(i*4,0)
+        pyautogui.moveTo(1,1)
+        for i in range(0,3):
+            pyautogui.press("shift")
+        print(f"Movement made at {datetime.now().time()}")
 
         if current_time > time_to_stop:
             iso_time = time.strftime("%H:%M:%S", time_to_stop)
             print(f"[ + ] It's after {iso_time} now, stopping this script.")
             condition_to_run = False
 
-        # Interval in seconds
-        if time_interval is None:
-            time.sleep(15)
-        else:
-            time.sleep(float(time_interval))
-
 
 def main():
     print(banner)
     parser = argparse.ArgumentParser()
-    parser.add_argument("time", help="Please enter the time in HH:MM:SS format")
+    parser.add_argument("time", help="Please enter the time in HH:MM:SS format. E.g: 23:00:00")
     parser.add_argument(
         "-i",
         "--interval",
@@ -85,12 +72,12 @@ def main():
         current_date = time.strftime("%Y %m %d")
         args.time = time.strptime(f"{current_date} {args.time}", "%Y %m %d  %H:%M:%S")
     except ValueError:
-        print(f"[ - ] The time you entered is incorrect. Try again in HH:MM:SS format")
+        print(f"[-] The time you entered is incorrect. Try again in HH:MM:SS format")
 
     try:
         runMouse(args.time, args.interval)
     except KeyboardInterrupt:
-        print(f"[ + ] The bot is stopped by the user. Good bye!")
+        print(f"[!] The bot is stopped by the user. Good bye!")
         sys.exit()
 
 
